66cfe6886572bfbea240c7b811a740cc0dbeb0d6
Pete Maynard <pgm987@gmail.com>
2012-10-27 18:58:14 +0100

    Added SQL, Feed Retriever

---
+++ rss.php
@@ -0,0 +1,29 @@
+
+<?php
+
+$link = mysqli_connect("localhost", "root", "superawesomepass", "port22feeds");
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$query = "SELECT title, url FROM feeds ORDER BY timestamp DESC;";
+
+if ($result = mysqli_query($link, $query)) {
+
+    /* fetch associative array */
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<a href=\"{$row["url"]}\">{$row["title"]}</a><br/>\n";
+    }
+
+    /* free result set */
+    mysqli_free_result($result);
+}
+
+/* close connection */
+mysqli_close($link);
+
+
+?>
