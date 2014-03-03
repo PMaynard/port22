45ea00458fb98401bd3155e5cf51cc0d9859ec48
Pete <pgm987@gmail.com>
2014-03-03 01:24:42 +0000

    Now able to retrieve and store feeds, as well as update via socket.

--- README.md
+++ README.md
@@ -4,18 +4,19 @@ Port 22
 This is the source code for port 22, security news feed.
 
 ## Features
-Runs a self contained __node__ server, and aggregates [RSS]() and [Atom]() feeds into a clean and simple web interface. With __real-time updates__ using [JQuery]() and [WebSockets]().
+Runs a self contained __node__ server, and aggregates [RSS](https://en.wikipedia.org/wiki/Rss) and [Atom](https://en.wikipedia.org/wiki/Atom_%28standard%29) feeds into a clean and simple web interface. With __real-time updates__ using [JQuery](http://jquery.com/) and [WebSockets](http://www.websocket.org/).
 
 ## Usage
 Run as a user who has privileges to listen on port 80. e.g. root.
 >node port22.js
 
 ## Dependencies 
-- [Socket.io]() 
-- [Express]() 
+- [Socket.io](http://socket.io) 
+- [Express](http://expressjs.com) 
 - [MySQl](https://npmjs.org/package/mysql)
+- [FeedParser](https://github.com/danmactough/node-feedparser)
 
-> npm install socket.io express mysql
+> npm install socket.io express mysql feedparser
 
 ## Install
 
@@ -23,4 +24,4 @@ Run as a user who has privileges to listen on port 80. e.g. root.
     mysql -u root < init-db.sql
     gunzip port22.sql.gz
     mysql -u root < port22.sql
-    npm install socket.io express mysql
+    npm install socket.io express mysql feedparser
--- port22.js
+++ port22.js
@@ -2,8 +2,10 @@ var app       = require('express')()
   , server    = require('http').createServer(app)
   , io        = require('socket.io').listen(server)
   , mysql     = require('mysql')
+  , FeedParser 	= require('feedparser')
+  , request   = require('request')
   // Application configuration file.
-  , config    = require('./config/config.json');
+  , config    = require('./config/config.json')
 
 var connection = mysql.createConnection({
   host     : config.hostname,
@@ -18,9 +20,57 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/html/index.html');
 });
 
+app.get('/update', function (req, res) {
+  proccessFeeds();
+  res.send('Done');
+});
+
 io.sockets.on('connection', function (socket) {
-  connection.query('SELECT title,url FROM feeds LIMIT 5', function(err, rows, fields) {
+  connection.query('SELECT title, url FROM feeds ORDER by id desc LIMIT 5', function(err, rows, fields) {
   if (err) throw err;
       socket.emit('init', rows);
   });
 });
+
+function addFeedItem(title, url, timestamp, hash) {
+	if(!timestamp) timestamp = "NOW()";
+
+	var post  = {title: title, url: url, timestamp: timestamp, hash: hash};
+	connection.query('INSERT INTO feeds SET ?', post , function(err, result) {
+		if(err){}
+			//console.log(err);
+			
+		if(undefined != result){
+			//console.log(result);
+			io.sockets.emit('update', [{"title":title, "url":url}]);
+		}
+	});
+}
+
+function parseFeed(feed_url) {
+	var req = request(feed_url), 
+		feedparser = new FeedParser();
+
+	req.on('error', function (error) { console.log("Request opa."); });
+
+	req.on('response', function (res) {
+		var stream = this;
+		if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+		stream.pipe(feedparser);
+	});
+
+	feedparser.on('error', function(error) { console.log("Feedparser opa."); });
+
+	feedparser.on('readable', function() {
+		var stream = this, item;
+		while (item = stream.read()) {
+	    	addFeedItem(item.title, item.link, item.date, require('crypto').createHash('md5').update(item.title + item.link).digest("hex"))
+		}
+	});
+}
+
+function proccessFeeds() {
+	for(var i in config.feeds_rss) {
+		parseFeed(config.feeds_rss[i]);
+	}
+}
