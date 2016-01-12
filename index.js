7d0b296b9f3e4cac633c468e5cec4b95d413f27d
Pete <pgm987@gmail.com>
2016-01-12 15:38:19 +0000

    Fix setInterval

--- index.js
+++ index.js
@@ -11,85 +11,78 @@ var util = require("util");
 var feed = require("feed-read");
 
 var MAX_NUMBER_REQUEST = 30;
+var PORT = 8080;
 
+/* Database Indeices */
 db_news.ensureIndex({ fieldName: 'title',  unique: true }, err_constraints());
 db_news.ensureIndex({ fieldName: 'author' }, err_constraints());
 db_news.ensureIndex({ fieldName: 'source_url' }, err_constraints());
 
 db_urls.ensureIndex({ fieldName: 'url',  unique: true }, function() {});
 
-check_feeds();
+function init(){
+	io.serveClient(false);
+	var server = io.listen(PORT);
+	util.log("Server listening on port " + PORT);
 
-/* Listen for websocket connections */
-io.serveClient(false);
-io.listen(8080);
-util.log("Server listening on port 8000.");
+	io.on('connection', function(client) {
+		client.on('get_articles', function(data) {
+			get_articles(client, data.n);
+		});
 
-/* Check feeds every 5min (300,000ms) */
-//setTimeout(check_feeds, 300000);
-setTimeout(check_feeds, 10000);
-
-io.on('connection', function(client) {
-	client.on('get_articles', function(data) {
-		get_articles(client, data.n);
+		client.on("disconnect", function() {
+			util.log("Client disconnected: " + client.id);
+		});
 	});
 
-	client.on("disconnect", function() {
-		util.log("Client disconnected: " + client.id);
-	});
-});
+	/* Check feeds every 5min (300,000ms) */
+	setInterval(check_feeds, 300000);
+}
 
 function check_feeds() {
 	util.log("Database Update...");
 	db_urls.find({}, function (err, feed_list) {
 		if(err){
-			util.log(err.message)
-		}else{
-			for(var i in feed_list) {
-				parase_feed(feed_list[i].url, feed_list[i].name)
-			}
+			util.log("ERROR: " + err.message);
+			return;
 		}
-	});
+		for (var i in feed_list) {
+			(function(i) {
+				feed(feed_list[i].url, function(err, articles) {
+					if (err){
+						util.log(err);
+						return;
+					}
+					for(var k in articles){
+						(function(k) {
+							var article = {
+								title: articles[k].title
+								, author: articles[k].author
+								, link: articles[k].link
+								, content: articles[k].content
+								, published: articles[k].published
+								, source_url : feed_list[i].url
+								, source_name : feed_list[i].name
+							};
+
+							db_news.insert(article, function (err, res) {
+								if(err && err.errorType === 'uniqueViolated' ) {
+									// No need to catch this, we're using constraints to prevent adding duplicates.
+									// util.log("Data already there.")
+								}else if(err){
+									util.log("ERROR: [" + err.errorType + "] " + err.message);
+								}else{
+									io.emit('article', res);
+									util.log(res.title);
+								};
+							}); // End insert 
+	   					})(k); // End K function
+	   				} // End for k articles 
+				}); // End feed function
+			})(i); // End function i
+		} // End Feed For
+	}); // End .find.
 	util.log("Database Update Complete."); 
-}
-
-function parase_feed(url, name) {
-	util.log("DEBUG: " + name);
-	feed(url, function(err, articles) {
-		if (err){
-			util.log(err);
-		}else{
-			add_articles(url, name, articles);
-		};
-	});
-}
-
-function add_articles(url, name, articles){
-	// console.log(name)
-
-	for(var k in articles){
-		var article = {
-			title: articles[k].title
-			, author: articles[k].author
-			, link: articles[k].link
-			, content: articles[k].content
-			, published: articles[k].published
-			, source_url : url
-			, source_name : name
-		};
-
-		db_news.insert(article, function (err, res) {
-			if(err && err.errorType === 'uniqueViolated' ) {
-				// No need to catch this, we're using constraints to prevent adding duplicates.
-				// util.log("Data already there.")
-			}else if(err){
-				util.log("ERROR: [" + err.errorType + "] " + err.message);
-			}else{
-				io.emit('article', res);
-				util.log(res.title);
-			};
-		});
-	}
 }
 
 function get_articles(client, n) {
@@ -120,3 +113,5 @@ function add_feed(url, name){
 	console.log(feed)
 	db_urls.insert(feed, function (err, res) { });
 }
+
+init();
