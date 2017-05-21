9c6f75d314aa30acc629ff479e4da836478f8761
Pete Maynard <pgm987@gmail.com>
2017-05-21 15:32:17 +0100

    Don't overwrite if file exists

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ var Datastore = require('nedb')
 var db_news = new Datastore({ filename: 'data/db_news', autoload: true, timestampData: true, inMemoryOnly: false});
 var db_urls = new Datastore({ filename: 'data/db_urls', autoload: true, timestampData: true });
 
-var io = require('socket.io')();
 var util = require("util");
 var feed = require("feed-read");
 
 var fs = require('fs');
 var mst = require('mustache');
-
-var MAX_NUMBER_REQUEST = 30;
-var PORT = 8010;
 
 /* Database Indeices */
 db_news.ensureIndex({ fieldName: 'title',  unique: true }, err_constraints());
@@ -24,36 +20,19 @@ db_news.ensureIndex({ fieldName: 'source_url' }, err_constraints());
 db_urls.ensureIndex({ fieldName: 'url',  unique: true }, function() {});
 
 function init(){
-	// io.serveClient(false);
-	// var server = io.listen(PORT);
-	// util.log("Server listening on port " + PORT);
-
-	// io.on('connection', function(client) {
-	// 	util.log("Connection: " + client);
-	// 	client.on('get_articles', function(data) {
-	// 		get_articles(client, data.n, data.feed);
-	// 	});
-
-	// 	client.on('get_feeds_list', function() {
-	// 		get_feeds(client);
-	// 	});
-
-	// 	client.on("disconnect", function() {
-	// 		util.log("Client disconnected: " + client.id);
-	// 	});
-	// });
-
 	// /* Check feeds every 5min (300,000ms) 40min (2,400,000) */
-	setInterval(check_feeds, 3000000);
 	dump_data();
+	// setInterval(check_feeds, 3000000);
 }
 
 function dump_data() {
+	util.log("Starting DataDump..")
 	db_news.find({}).sort({ createdAt: -1 }).exec( function (err, docs) {
 		for(doc in docs){
 			fileLoc = "../port22-static/content/news/" + docs[doc]._id + ".md"
 			if(fs.existsSync(fileLoc)){
-				return;
+				// util.log("Skipping file", fileLoc)
+				continue;
 			}
 			var template = '\n\
 ---\n\
@@ -82,13 +61,14 @@ db_createdAt: "{{ createdAt }}"\n\
 
 			var out = mst.render(template, data)
 			// util.log(docs)
-			util.log(out)
+			util.log(docs[doc].title)
 			fs.writeFile(fileLoc, out, function(err) {
 			    if(err) {
 			        return util.log(err);
 			    }
 			});
 		}
+		util.log("DataDump Complete.");
 	});
 }
 
@@ -138,32 +118,6 @@ function check_feeds() {
 	}); // End .find.
 	util.log("Database Update Complete.");
 	dump_data();
-}
-
-function get_articles(client, n, feed) {
-	if(n >= MAX_NUMBER_REQUEST){
-		n = MAX_NUMBER_REQUEST;
-	}else if(n <= 0){
-		n = 1;
-	}
-
-	if(feed){
-		db_news.find({source_name: feed}).sort({ createdAt: -1 }).limit(n).exec( function (err, docs) {
-			client.emit('articles', docs.reverse());
-			util.log("Handling get_articles: " + feed);
-		});	
-	}else{
-		db_news.find({}).sort({ createdAt: -1 }).limit(n).exec( function (err, docs) {
-			client.emit('articles', docs.reverse());
-			util.log("Handling get_articles: " + n)
-		});
-	}
-}
-
-function get_feeds(client) {
-        db_urls.find({}, function (err, docs) {
-                client.emit('feed_list', docs);
-        });
 }
 
 /* Helper Functions */
